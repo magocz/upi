59bd3f3660013f7fa167fb82660c47ae30f7b236
Neldam <msgocz@gmial.com>
2016-07-06 22:46:40 +0200

    test gpio

--- server/src/main/java/UPIControllImpl.java
+++ server/src/main/java/UPIControllImpl.java
@@ -8,7 +8,7 @@ public class UPIControllImpl extends UnicastRemoteObject implements UPIControll 
 
     public UPIControllImpl() throws RemoteException {
         Gpio.wiringPiSetup();
-        SoftPwm.softPwmCreate(0, 0, 50);
+        SoftPwm.softPwmCreate(0, 0, 25);
     }
 
     public void setValueOnPin_0(Integer value) {
