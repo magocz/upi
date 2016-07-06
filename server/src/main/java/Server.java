1ebf8d7982785671745c0a6d057e2f406bffe86c
Neldam <msgocz@gmial.com>
2016-07-07 00:00:28 +0200

    test gpio

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -5,8 +5,20 @@ public class Server {
     public static void main(String args[]) throws InterruptedException {
 
         System.setProperty("java.rmi.server.hostname", "192.168.1.31");
+        com.pi4j.wiringpi.Gpio.wiringPiSetupGpio();
 
-       try {
+        com.pi4j.wiringpi.Gpio.pinMode(18, com.pi4j.wiringpi.Gpio.PWM_OUTPUT);
+        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
+        com.pi4j.wiringpi.Gpio.pwmSetClock(384);
+        com.pi4j.wiringpi.Gpio.pwmSetRange(1000);
+        com.pi4j.wiringpi.Gpio.pwmWrite(18, 75);
+
+        com.pi4j.wiringpi.Gpio.pinMode(19, com.pi4j.wiringpi.Gpio.PWM_OUTPUT);
+        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
+        com.pi4j.wiringpi.Gpio.pwmSetClock(384);
+        com.pi4j.wiringpi.Gpio.pwmSetRange(1000);
+        com.pi4j.wiringpi.Gpio.pwmWrite(19, 75);
+        try {
             int host = 7007;
             UPIControll obj = new UPIControllImpl();
             // Bind the remote object's stub in the registry
