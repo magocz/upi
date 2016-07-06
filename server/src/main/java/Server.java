8b3f4ad48750efe810c816802aac5db9efff2e84
Neldam <msgocz@gmial.com>
2016-07-06 23:20:17 +0200

    test gpio

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,26 +1,23 @@
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import com.pi4j.io.gpio.*;
 
 
 public class Server {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
 
-        com.pi4j.wiringpi.Gpio.wiringPiSetupGpio();
+        System.setProperty("java.rmi.server.hostname", "192.168.1.31");
+        final GpioController gpio = GpioFactory.getInstance();
 
-        com.pi4j.wiringpi.Gpio.pinMode(18, com.pi4j.wiringpi.Gpio.PWM_OUTPUT);
-        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
-        com.pi4j.wiringpi.Gpio.pwmSetClock(384);
-        com.pi4j.wiringpi.Gpio.pwmSetRange(1000);
-        com.pi4j.wiringpi.Gpio.pwmWrite(18, 75);
+        // provision gpio pin #01 as an output pin and turn on
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
+        while (true) {
 
-        com.pi4j.wiringpi.Gpio.pinMode(19, com.pi4j.wiringpi.Gpio.PWM_OUTPUT);
-        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
-        com.pi4j.wiringpi.Gpio.pwmSetClock(384);
-        com.pi4j.wiringpi.Gpio.pwmSetRange(1000);
-        com.pi4j.wiringpi.Gpio.pwmWrite(19, 75);
+            pin.high();
+            Thread.sleep(0, 1000);
+            pin.low();
+            Thread.sleep(19);
 
-        /*System.setProperty("java.rmi.server.hostname", "192.168.1.31");
-        try {
+        }
+      /*  try {
             int host = 7007;
             UPIControll obj = new UPIControllImpl();
             // Bind the remote object's stub in the registry
