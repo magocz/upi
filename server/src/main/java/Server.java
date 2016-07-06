f8435ddf378e2c7470bae4febd3e58ede812d1ae
Neldam <msgocz@gmial.com>
2016-07-06 23:54:24 +0200

    test gpio

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,21 +1,12 @@
-import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.impl.PinImpl;
-
-import java.util.EnumSet;
-
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Server {
     public static void main(String args[]) throws InterruptedException {
 
         System.setProperty("java.rmi.server.hostname", "192.168.1.31");
-        final GpioController gpio = GpioFactory.getInstance();
-        GpioPinPwmOutput pwmOutput = gpio.provisionPwmOutputPin(RaspiPin.GPIO_26);
-        pwmOutput.setPwm(25);
-        while (true) {
 
-
-        }
-      /*  try {
+       try {
             int host = 7007;
             UPIControll obj = new UPIControllImpl();
             // Bind the remote object's stub in the registry
@@ -26,6 +17,6 @@ public class Server {
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
-        }*/
+        }
     }
 }
--- server/src/main/java/UPIControllImpl.java
+++ server/src/main/java/UPIControllImpl.java
@@ -1,18 +1,23 @@
-import com.pi4j.wiringpi.Gpio;
-import com.pi4j.wiringpi.SoftPwm;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.RaspiPin;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class UPIControllImpl extends UnicastRemoteObject implements UPIControll {
 
+    private GpioPinPwmOutput pwmOutput;
+
     public UPIControllImpl() throws RemoteException {
-        Gpio.wiringPiSetup();
-        SoftPwm.softPwmCreate(0, 0, 20);
+        final GpioController gpio = GpioFactory.getInstance();
+        pwmOutput = gpio.provisionPwmOutputPin(RaspiPin.GPIO_26);
+
     }
 
     public void setValueOnPin_0(Integer value) {
         System.out.println("Value: " + value);
-        SoftPwm.softPwmWrite(0, value);
+        pwmOutput.setPwm(value);
     }
 }
