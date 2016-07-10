d6c2de5190941ef15246ff8f105f84c7c4eb94e2
Neldam <msgocz@gmial.com>
2016-07-10 21:47:01 +0200

    test gpio

--- client/src/main/java/App.java
+++ client/src/main/java/App.java
@@ -1,23 +1,55 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class App {
+public class App extends JPanel {
 
-    public static void main(String[] args) {
+    private static UPIControll stub;
+    public App() {
+        super(true);
         try {
             int host = 7007;
-            Registry registry = LocateRegistry.getRegistry("83.7.213.20", host);
-            UPIControll stub = (UPIControll) registry.lookup("upi_server");
-            /*for(int i = 500 ; i < 1000 ; i++){
-                stub.setValueOnPin_0(i);
-                Thread.sleep(500);
-            }*/
-            stub.setValueOnPin_0(1000);
-
-
+            Registry registry = LocateRegistry.getRegistry("83.30.122.162", host);
+            stub = (UPIControll) registry.lookup("upi_server");
+            stub.setValueOnPin_0(13);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         }
+        this.setLayout(new BorderLayout());
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 4, 25, 13);
+
+        slider.setMinorTickSpacing(2);
+        slider.setMajorTickSpacing(10);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+        // We'll just use the standard numeric labels for now...
+        slider.setLabelTable(slider.createStandardLabels(10));
+
+        add(slider, BorderLayout.CENTER);
+
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent ce) {
+                try {
+                    stub.setValueOnPin_0(((JSlider) ce.getSource()).getValue());
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Slider Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new App());
+        frame.pack();
+        frame.setVisible(true);
     }
 }
